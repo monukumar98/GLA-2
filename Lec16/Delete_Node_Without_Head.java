501496daba098123eb7fabb071fbb593b5641821
monukumar98 <monu012as@gmail.com>
2022-01-28 17:09:38 +0530

    LinkedList-qps

--- Lec16/Delete_Node_Without_Head.java
+++ Lec16/Delete_Node_Without_Head.java
@@ -1,5 +1,20 @@
 package Lec16;
 
 public class Delete_Node_Without_Head {
+	public class ListNode {
+		int val;
+		ListNode next;
 
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+		public void deleteNode(ListNode node) {
+			node.val=node.next.val;
+			node.next=node.next.next;
+
+		}
+	}
 }
