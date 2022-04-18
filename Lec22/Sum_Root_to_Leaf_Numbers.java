a3c2160ec20180d39cb44c9ec64a3e718beb6bc0
monukumar98 <monu012as@gmail.com>
2022-04-18 16:12:00 +0530

    BT_Qps

---
+++ Lec22/Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,14 @@
+package Lec22;
+
+
+public class Sum_Root_to_Leaf_Numbers {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
